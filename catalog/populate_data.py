ce508b38e1fe926750f937c60f869908445eb841
Rob Monday <rob.monday@gmail.com>
2016-06-02 08:59:12 -0500

    Update populate_data.py to use user_id=1 instead of user_id=0

--- catalog/populate_data.py
+++ catalog/populate_data.py
@@ -20,27 +20,27 @@ user2 = User(name='Rob Monday', email='rob.monday@gmail.com')
 session.add(user2)
 session.commit()
 
-studio1 = Floorplan(name='Cozy Studio', square_footage=550, bedrooms=1, bathrooms=1, user_id=0)
+studio1 = Floorplan(name='Cozy Studio', square_footage=550, bedrooms=1, bathrooms=1, user_id=1)
 session.add(studio1)
 session.commit()
 
-studio2 = Floorplan(name='Spacious Studio', square_footage=750, bedrooms=1, bathrooms=1, user_id=0)
+studio2 = Floorplan(name='Spacious Studio', square_footage=750, bedrooms=1, bathrooms=1, user_id=1)
 session.add(studio2)
 session.commit()
 
-one_bedroom = Floorplan(name='Standard', square_footage=775, bedrooms=1, bathrooms=1, user_id=0)
+one_bedroom = Floorplan(name='Standard', square_footage=775, bedrooms=1, bathrooms=1, user_id=1)
 session.add(one_bedroom)
 session.commit()
 
-loft = Floorplan(name='Loft', square_footage=695, bedrooms=1, bathrooms=1, user_id=0)
+loft = Floorplan(name='Loft', square_footage=695, bedrooms=1, bathrooms=1, user_id=1)
 session.add(loft)
 session.commit()
 
-two_bedroom1 = Floorplan(name='Cozy Two-Bed', square_footage=900, bedrooms=2, bathrooms=1, user_id=0)
+two_bedroom1 = Floorplan(name='Cozy Two-Bed', square_footage=900, bedrooms=2, bathrooms=1, user_id=1)
 session.add(two_bedroom1)
 session.commit()
 
-two_bedroom2 = Floorplan(name='Large Two-Bed', square_footage=1150, bedrooms=2, bathrooms=2, user_id=0)
+two_bedroom2 = Floorplan(name='Large Two-Bed', square_footage=1150, bedrooms=2, bathrooms=2, user_id=1)
 session.add(two_bedroom2)
 session.commit()
 
