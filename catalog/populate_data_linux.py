287366e4de8ff8197f618d7a8f4f135a471f87a5
Rob Monday <rob.monday@gmail.com>
2016-06-02 09:36:58 -0500

    Update populate_data_linux to use manual repetitive data entry to avoid difficult error

--- catalog/populate_data_linux.py
+++ catalog/populate_data_linux.py
@@ -48,24 +48,65 @@ print "Floorplan data populated successfully!"
 
 # Populate Unit Data
 
-try:
-	json_data_file = '/var/www/Catalog/catalog/sample_data.json' #edited to reflect Linux folder structure
-	print len(sys.argv)
-	if len(sys.argv)==2:
-		json_data_file = '/var/www/Catalog/catalog/'+sys.argv[1] #edited to reflect Linux folder structure
+# try:
+# 	json_data_file = '/var/www/Catalog/catalog/sample_data.json' #edited to reflect Linux folder structure
+# 	print len(sys.argv)
+# 	if len(sys.argv)==2:
+# 		json_data_file = '/var/www/Catalog/catalog/'+sys.argv[1] #edited to reflect Linux folder structure
 
-	with open(json_data_file) as json_data:  # to see much more data, try:  sample_data_extended.json
-	    dataload = json.load(json_data)
-	    units = dataload['Unit']
-	    # print units
-	    iteration_num = 0
-	    for unit in units:
-	    	iteration_num += 1
-	    	print 'Add unit # '+str(iteration_num)
-	    	entry = Unit(name=unit['name'], floorplan_id=unit['floorplan_id'], user_id=unit['user_id'])
-	    	session.add(entry)
-	    	session.commit()
-	print "Unit data populated successfully!"
-except:
-	print "JSON did not load"
+# 	with open(json_data_file) as json_data:  # to see much more data, try:  sample_data_extended.json
+# 	    dataload = json.load(json_data)
+# 	    units = dataload['Unit']
+# 	    # print units
+# 	    iteration_num = 0
+# 	    for unit in units:
+# 	    	iteration_num += 1
+# 	    	print 'Add unit # '+str(iteration_num)
+# 	    	entry = Unit(name=unit['name'], floorplan_id=unit['floorplan_id'], user_id=unit['user_id'])
+# 	    	session.add(entry)
+# 	    	session.commit()
+# 	print "Unit data populated successfully!"
+# except:
+# 	print "JSON did not load"
 
+entry = Unit(name='Unit #1', floorplan_id=1, user_id=1)
+session.add(entry)
+session.commit()
+
+entry = Unit(name='Unit #2', floorplan_id=1, user_id=1)
+session.add(entry)
+session.commit()
+
+entry = Unit(name='Unit #3', floorplan_id=1, user_id=1)
+session.add(entry)
+session.commit()
+
+entry = Unit(name='Unit #4', floorplan_id=1, user_id=1)
+session.add(entry)
+session.commit()
+
+entry = Unit(name='Unit #5', floorplan_id=2, user_id=1)
+session.add(entry)
+session.commit()
+
+entry = Unit(name='Unit #6', floorplan_id=2, user_id=1)
+session.add(entry)
+session.commit()
+
+entry = Unit(name='Unit #7', floorplan_id=3, user_id=1)
+session.add(entry)
+session.commit()
+
+entry = Unit(name='Unit #8', floorplan_id=4, user_id=1)
+session.add(entry)
+session.commit()
+
+entry = Unit(name='Unit #9', floorplan_id=4, user_id=1)
+session.add(entry)
+session.commit()
+
+entry = Unit(name='Unit #10', floorplan_id=5, user_id=1)
+session.add(entry)
+session.commit()
+
+print "Unit data populated in repetitive manner--not from JSON file"
